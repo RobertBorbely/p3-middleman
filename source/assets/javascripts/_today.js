48ca3ae6a76b606eacbab0fe6db2d92e59533d43
Róbert Borbély <borbelyr@borbelyr.local>
2018-03-07 18:42:24 +0100

    Add mobile-menu and add js files

--- source/assets/javascripts/_today.js
+++ source/assets/javascripts/_today.js
@@ -5,3 +5,10 @@ $(function() {
   var date = moment().format('dddd, MMMM Do, YYYY');
   $('.today').html('<p style="color: blue">Hello from jQuery land!<br/>Moment.js says today is ' + date + '.</p>');
 });
+
+$(document).ready(function(){
+  $('.menu-trigger,.fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.mobile-menu-content,.fade-screen, main').toggleClass('menu-open');
+    e.preventDefault();
+  });
+});
